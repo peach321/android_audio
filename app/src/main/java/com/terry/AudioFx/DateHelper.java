795b3a79e352cc9f44a2dbeda27c792dd32f87fa
peach321 <1161944233@qq.com>
2017-09-14 17:33:35 +0800

    add date helper

---
+++ app/src/main/java/com/terry/AudioFx/DateHelper.java
@@ -0,0 +1,66 @@
+package com.terry.AudioFx;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/* A simple datehelper for building & converting date from & to strings
+ * Created by liulingfeng on 2017/4/11.
+ */
+
+public class DateHelper {
+    static private DateHelper instance;
+
+    private DateHelper(){
+
+    }
+
+    static public DateHelper getInstance(){
+        if (instance == null) {
+            instance = new DateHelper();
+        }
+        return instance;
+    }
+
+    public Date buildDate(String dateStr){
+        DateFormat format = new SimpleDateFormat("yyyy-mm-dd-hh-mm");
+        try {
+            return format.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public int getYear(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.YEAR);
+    }
+
+    public int getMonth(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.MONTH);
+    }
+
+    public int getDay(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public int getHour(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.HOUR_OF_DAY);
+    }
+
+    public int getMin(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.MINUTE);
+    }
+}
